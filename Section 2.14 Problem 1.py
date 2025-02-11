54810580c9be97ad86384d1d07a176a5b90e9db2
Grant Spach <grantdspach@gmail.com>
2025-02-10 22:08:52 -0700

    Add files via upload

---
+++ Section 2.14 Problem 1.py
@@ -0,0 +1,14 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a="All"
+>>> b="work"
+>>> c="and"
+>>> d="no"
+>>> e="play"
+>>> f="makes"
+>>> g="Jack"
+>>> h="a"
+>>> i="dull"
+>>> j="boy"
+>>> print(a,b,c,d,e,f,g,h,i,j)
+All work and no play makes Jack a dull boy
