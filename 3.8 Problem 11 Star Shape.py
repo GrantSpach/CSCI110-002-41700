5daecf8a37ad69e0f553e3f193245b3735ebe0b2
Grant Spach <grantdspach@gmail.com>
2025-02-17 22:26:46 -0700

    Add files via upload

---
+++ 3.8 Problem 11 Star Shape.py
@@ -0,0 +1,42 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      grant
+#
+# Created:     17/02/2025
+# Copyright:   (c) grant 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+
+
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+wn.title("Star")
+
+grant = turtle.Turtle()
+grant.shape("turtle")
+grant.color("blue")
+grant.speed(5)
+
+grant.forward(50)
+grant.left(144)
+grant.forward(50)
+grant.left(144)
+grant.forward(50)
+grant.left(144)
+grant.forward(50)
+grant.left(144)
+grant.forward(50)
+grant.left(144)
+
+wn.mainloop()
