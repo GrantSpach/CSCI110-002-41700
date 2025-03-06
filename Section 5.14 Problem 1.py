991bef820f8fae07f2a3c31c0f0b515bec820fbf
Grant Spach <grantdspach@gmail.com>
2025-03-06 01:06:41 -0700

    Add files via upload

---
+++ Section 5.14 Problem 1.py
@@ -0,0 +1,39 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      grant
+#
+# Created:     06/03/2025
+# Copyright:   (c) grant 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+
+
+def days_of_week (day_number):
+    if day_number == 0:
+        print("Sunday")
+    elif day_number == 1:
+        print("Monday")
+    elif day_number == 2:
+        print("Tuesday")
+    elif day_number == 3:
+        print("Wednesday")
+    elif day_number == 4:
+        print("Thursday")
+    elif day_number == 5:
+        print("Friday")
+    elif day_number == 6:
+        print("Saturday")
+
+
+days_of_week(2)
+
+
